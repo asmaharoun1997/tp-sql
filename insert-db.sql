83525821e0bf76f1fe06d7288391ccb335bb8f80
asma <asmaharoun97@gmail.com>
2026-07-28 11:15:42 +0200

    ajout de laffichage des fichiers avec jenkinsfile

--- insert-db.sql
+++ insert-db.sql
@@ -2,5 +2,5 @@ USE geoDB;
 
 INSERT INTO departement (num, nom, prefecture) VALUES
 (94, 'Val-de-Marne', 'Créteil'),
-(93, 'Seine-Saint-Denis', 'Bobigny');
-(75, 'Paris', 'Paris'),
+(93, 'Seine-Saint-Denis', 'Bobigny'),
+(75, 'Paris', 'Paris');
